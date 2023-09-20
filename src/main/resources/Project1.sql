9b88d30bf6a4b45e5b2dd4bbc660fb82ef875603
Bryton Tateishi <brytontateishi@gmail.com>
2023-09-20 11:47:38 -0700

    added SQL file

--- src/main/resources/Project1.sql
+++ src/main/resources/Project1.sql
@@ -13,24 +13,14 @@ CREATE TABLE accounts(
     account_id serial PRIMARY KEY,
     account_type TEXT NOT NULL,
     account_balance DECIMAL NOT NULL,
-    user_id TEXT REFERENCES users(user_id)
+    user_id int REFERENCES users(user_id)
 );
 
-
-INSERT INTO users(username, first_name, last_name, user_password, user_admin) VALUES ('jdoe', 'John', 'Doe', 'password', FALSE);
-INSERT INTO users(username, first_name, last_name, user_password, user_admin) VALUES ('theSponge', 'Spongebob', 'Squarepants', 'word', FALSE);
-INSERT INTO users(username, first_name, last_name, user_password, user_admin) VALUES ('clarinetman', 'Squidward', 'Tentacles', 'pass', FALSE);
-INSERT INTO users(username, first_name, last_name, user_password, user_admin) VALUES ('funnymoneyman', 'Eugene', 'Krabs', 'Formula', TRUE);
-INSERT INTO users(username, first_name, last_name, user_password, user_admin) VALUES ('betat', 'Bryton', 'Tateishi', 'Password', TRUE);
-
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('Checking', 100.20, 'jdoe');
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('Savings', 50.20, 'jdoe');
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('Checking', 10.20,'theSponge');
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('Retirement', 100, 'theSponge');
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('Checking', 250.20, 'clarinetman');
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('HSA', 300.60, 'funnymoneyman');
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('Savings', 1000.87, 'funnymoneyman');
-INSERT INTO accounts(account_type, account_balance, user_username) VALUES ('Checking', 100, 'betat');
-
+CREATE TABLE loan_apps(
+    loan_id serial PRIMARY KEY,
+    loan_amount DECIMAL NOT NULL,
+    approval BOOL NOT NULL,
+    account_id int REFERENCES account(account_id)
+)
 
 SELECT * FROM users, accounts;

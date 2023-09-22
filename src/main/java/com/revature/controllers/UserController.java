fc3700e8492a45f3c7fbd5192a001c78653cab3f
yantaozhu <yantao0825@gmail.com>
2023-09-22 13:48:22 -0400

    Added Get and Post for User

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.revature.controllers;
 
+import com.revature.models.User;
 import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -15,5 +19,31 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok().body(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable("id") int id) {
+        try {
+            return ResponseEntity.ok().body(userService.getUserById(id));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> insertUser(@RequestBody User user) {
+        try {
+            User insertedUser = userService.insertUser(user);
+            return ResponseEntity.accepted().body(insertedUser);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,8 +1,12 @@
 package com.revature.services;
 
 import com.revature.doas.UserDAO;
+import com.revature.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +16,35 @@ public class UserService {
     @Autowired
     public UserService(UserDAO userDAO) {
         this.userDAO = userDAO;
+    }
+
+    public List<User> getAllUsers() {
+        return userDAO.findAll();
+    }
+
+    public User getUserById(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("User with an id of 0 or less do not exist.");
+        }
+        Optional<User> user = userDAO.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new IllegalArgumentException("User with ID of " + id + " does not exist.");
+        }
+    }
+
+    public User insertUser(User user) throws IllegalArgumentException {
+        if (user.getUserUsername().equals(null) || user.getUserUsername().isEmpty()) {
+            throw new IllegalArgumentException("Username must not be empty!");
+        }
+        if (user.getUserPassword().equals(null) || user.getUserPassword().isEmpty()) {
+            throw new IllegalArgumentException("Password must not be empty!");
+        }
+        if (userDAO.findByUserUsername(user.getUserUsername()).isEmpty()) {
+            return userDAO.save(user);
+        } else {
+            throw new IllegalArgumentException("Username already exist!");
+        }
     }
 }
